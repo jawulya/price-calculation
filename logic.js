9a6c2037f77910b27464a1af77e582f1b4e292d7
Denys Faber <denys.faber@contractors.medallia.com>
2021-11-20 10:18:41 +0200

    moved course server to privat

--- logic.js
+++ logic.js
@@ -7,9 +7,9 @@ let EUR_RATE = 0;
 let USD_RATE = 0;
 let GPB_RATE = 0;
 let PLN_RATE = 0;
-
 function getCurrency() {
-    fetch('https://api.monobank.ua/bank/currency')  .then((response) => {
+    fetch(`https://api.privatbank.ua/p24api/exchange_rates?json&date=${new Date().toLocaleDateString('ru').replaceAll('/', '.')}`)
+        .then((response) => {
         if (!response.ok) {
             setTimeout(getCurrency, 5000);
             throw new Error('error');
@@ -18,10 +18,10 @@ function getCurrency() {
     })
         .then((data) => {
             document.getElementById('loading').style.display = 'none';
-            EUR_RATE = data.find((e) => e.currencyCodeA === EUR_CODE)?.rateSell;
-            USD_RATE = data.find((e) => e.currencyCodeA === USD_CODE)?.rateSell;
-            GPB_RATE = data.find((e) => e.currencyCodeA === GPB_CODE)?.rateCross;
-            PLN_RATE = data.find((e) => e.currencyCodeA === PLN_CODE)?.rateSell;
+            EUR_RATE = data.exchangeRate.find((e) => e.currency === 'EUR')?.saleRate;
+            USD_RATE = data.exchangeRate.find((e) => e.currency === 'USD')?.saleRate;
+            GPB_RATE = data.exchangeRate.find((e) => e.currency === 'GBP')?.saleRate;
+            PLN_RATE = data.exchangeRate.find((e) => e.currency === 'PLN')?.saleRate;
 
             document.getElementById('usd').innerHTML = USD_RATE
             document.getElementById('eur').innerHTML = EUR_RATE
@@ -30,6 +30,29 @@ function getCurrency() {
         })
         .catch(() => {});
 }
+
+// function getCurrency() {
+//     fetch('https://api.monobank.ua/bank/currency')  .then((response) => {
+//         if (!response.ok) {
+//             setTimeout(getCurrency, 5000);
+//             throw new Error('error');
+//         }
+//         return response.json();
+//     })
+//         .then((data) => {
+//             document.getElementById('loading').style.display = 'none';
+//             EUR_RATE = data.find((e) => e.currencyCodeA === EUR_CODE)?.rateSell;
+//             USD_RATE = data.find((e) => e.currencyCodeA === USD_CODE)?.rateSell;
+//             GPB_RATE = data.find((e) => e.currencyCodeA === GPB_CODE)?.rateCross;
+//             PLN_RATE = data.find((e) => e.currencyCodeA === PLN_CODE)?.rateSell;
+//
+//             document.getElementById('usd').innerHTML = USD_RATE
+//             document.getElementById('eur').innerHTML = EUR_RATE
+//             document.getElementById('pln').innerHTML = PLN_RATE
+//             document.getElementById('gpb').innerHTML = GPB_RATE
+//         })
+//         .catch(() => {});
+// }
 getCurrency()
 
 function calculatePriceFor(type, cost) {
